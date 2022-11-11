64e8e49723000011b88549add4dd1f0906598884
David Tainta <david.tainta@bosonit.com>
2022-11-11 09:24:49 +0100

    first commit

---
+++ src/main/java/com/pruebaTecnica/IMDb/ImDbApplication.java
@@ -0,0 +1,13 @@
+package com.pruebaTecnica.IMDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruebaTecnica/IMDb/ImDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebaTecnica.IMDb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
